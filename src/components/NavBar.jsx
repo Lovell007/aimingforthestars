c8979df7b1a12adc858e7b8335ed61aaa263daa1
james lovell <jameslovell@jamess-air.home>
2021-06-10 08:39:32 -0500

    some css

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom'
 
 export default function NavBar() {
   return (
-    <div>
+    <div id="navBar">
       <Link to="/solarSystem">Solar System</Link>
       <Link to="/">Planet Creator</Link>
       <Link to="/planetaryList">Planetary List</Link>
